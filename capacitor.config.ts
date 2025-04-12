b820ae5f3f50825ee3e09b0055e0a011ea7fbf75
raiben2123 <104256843+raiben2123@users.noreply.github.com>
2025-04-12 20:12:03 +0200

    Capacitor instalado

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.studynamic.app',
+  appName: 'Studynamic',
+  webDir: 'build'
+};
+
+export default config;
